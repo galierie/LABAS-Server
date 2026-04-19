c6b38a3934b979299608c2e9e16cb8fc073f9338
SHIROKAMIQQ <migzguiang@gmail.com>
2026-04-19 16:39:00 +0800

    refactor: kyc_auth as a function; encode img into base64 for return object

--- kyc_auth.py
+++ kyc_auth.py
@@ -1,51 +1,50 @@
 from mosip_auth_sdk.models import DemographicsModel
 from mosip_auth_sdk import MOSIPAuthenticator
 from dynaconf import Dynaconf
+from typing import Dict
 import numpy as np
 import base64
 import cv2
 
+# MUST HAVE EXACT PATH OF settings_files
 config = Dynaconf(settings_files=["./certs/config.toml"], environments=False)
 authenticator = MOSIPAuthenticator(config=config)
 
-# kyc auth
-demographics_data = DemographicsModel(dob="2023/02/08")
-response = authenticator.kyc(
-    individual_id="6874180926",
-    individual_id_type="UIN",
-    demographic_data=demographics_data,
-    consent=True,
-)
-response_body = response.json()
-decrypted_response = authenticator.decrypt_response(response_body)
-face_bytes = base64.b64decode(decrypted_response.pop("photo"))
+# Given UIN and DOB from decoded QR
+# Authenticate the QR code's data
+# Return demographic data and ID' photo
+def kyc_auth(uin: str, dob: str) -> Dict:
+    # User-1 UIN: 6874180926
+    # User-1 dob: 2023/02/08
 
-# shows demographic data
-print(f"DECRYPTED RESPONSE: {decrypted_response}")
+    # Retrieve demographic data via kyc
+    demographics_data = DemographicsModel(dob=dob)
+    response = authenticator.kyc(
+        individual_id=uin,
+        individual_id_type="UIN",
+        demographic_data=demographics_data,
+        consent=True,
+    )
+    response_body = response.json()
+    decrypted_response = authenticator.decrypt_response(response_body)
+    face_bytes = base64.b64decode(decrypted_response.pop("photo"))
 
-# attempt to decode image from face_bytes
-img = None
-offsets_to_try = [i for i in range(70, 85)]
-for offset in offsets_to_try:
-    face_as_np = np.frombuffer(face_bytes[offset:], dtype=np.uint8)
-    img = cv2.imdecode(face_as_np, cv2.IMREAD_COLOR)
-    if img is not None:
-        print(f"Found valid image at offset {offset}")
-        break
+    # Attempt to decode image from face_bytes
+    img = None
+    offsets_to_try = [i for i in range(70, 85)]
+    for offset in offsets_to_try:
+        face_as_np = np.frombuffer(face_bytes[offset:], dtype=np.uint8)
+        img = cv2.imdecode(face_as_np, cv2.IMREAD_COLOR)
+        if img is not None:
+            break
+    if img is None:
+        raise Exception("Could not decode image")
 
-if img is None:
-    print("ERROR: Could not decode image")
-    exit(1)
+    # Convert img->jpeg->base64
+    _, buffer = cv2.imencode(".jpg", img)
+    img_base64 = base64.b64encode(buffer).decode("utf-8")
 
-# show image in window and save to file when 's' is pressed
-# cv2.imshow("test", img)
-# k = cv2.waitKey(0)
-
-# if k == ord("s"):
-#     cv2.imwrite("./face.jpg", img)
-#     exit()
-
-
-# For headless, just save the image for now.
-cv2.imwrite("./face.jpg", img)
-print("Image saved as face.jpg")
+    return {
+        "demographics": decrypted_response,
+        "photo": img_base64
+    }   

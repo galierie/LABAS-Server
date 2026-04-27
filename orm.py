f1760543084626580a20fe75db63f517950b134c
Eriene Galinato <edgalinato@up.edu.ph>
2026-04-27 11:38:50 +0800

    refactor: move all sql tables to orm module

--- orm.py
+++ orm.py
@@ -1,7 +1,44 @@
+"""
+Define db models based on the created schema
+"""
+
 from sqlmodel import Field, SQLModel
+from typing import Optional
 
 class Voter(SQLModel, table=True):
-  __tablename__ = "voters"
-  uin: int = Field(primary_key=True)
-  precinct: str
-  voted: bool
+    __tablename__ = "voters"
+    uin: int = Field(primary_key=True)
+    precinct: str
+    voted: bool
+
+class Scope(SQLModel, table=True):
+    scope_id: int = Field(primary_key=True)
+    scope_name: str
+
+class Position(SQLModel, table=True):
+    position_id: int = Field(primary_key=True)
+    position_name: str
+    scope_id: int = Field(foreign_key="scope.scope_id")
+    max_votes: int = Field(default=1)
+
+class Province(SQLModel, table=True):
+    __tablename__ = "provinces"
+    province_id: str = Field(primary_key=True)
+    province_name: str
+
+class City(SQLModel, table=True):
+    __tablename__ = "cities"
+    city_id: str = Field(primary_key=True)
+    city_name: str
+    province_id: str = Field(foreign_key="provinces.province_id")
+
+class Candidate(SQLModel, table=True):
+    __tablename__ = "candidates"
+    candidate_id: int = Field(primary_key=True)
+    candidate_name: str
+    party: str | None = None
+    candidate_number: int
+    position_id: int = Field(foreign_key="position.position_id")
+    province_id: Optional[str] = Field(default=None, foreign_key="provinces.province_id")
+    city_id: Optional[str] = Field(default=None, foreign_key="cities.city_id")
+    barangay_id: Optional[str] = Field(default=None, foreign_key="barangays.barangay_id")
